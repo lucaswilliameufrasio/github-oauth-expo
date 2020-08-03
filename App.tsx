df12498c8f1caf72374698edd463374bfb601cce
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-02 22:59:05 -0300

    feat: add SignIn with Github

--- App.tsx
+++ App.tsx
@@ -1,12 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Button } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import {
+  makeRedirectUri,
+  useAuthRequest,
+  getRedirectUrl,
+} from "expo-auth-session";
+import axios from "axios";
+import env from "./config/oauth";
 
 export default function App() {
+  WebBrowser.maybeCompleteAuthSession();
+  const redirectTo = getRedirectUrl();
+
+  const { config: githubConfig, discovery: githubDiscovery } = env.github;
+  githubConfig.redirectUri = makeRedirectUri({
+    native: redirectTo,
+  });
+
+  const [githubRequest, githubResponse, promptGithubAsync] = useAuthRequest(
+    githubConfig,
+    githubDiscovery
+  );
+
+  async function getAccessToken(code: string) {
+    // Usa esse código na API, não aqui
+    // A requisição deve ser realizada no backend
+    const token = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {},
+      {
+        params: {
+          code,
+          client_id: "356660bcfa4691481031",
+          client_secret: "",
+        },
+      }
+    );
+    console.log(token.data);
+  }
+
+  function handleGithubResponse() {
+    if (githubResponse?.type === "success") {
+      const { code } = githubResponse.params;
+      console.log(code);
+      console.log(githubResponse);
+      getAccessToken(code);
+    } else {
+      console.log(githubResponse);
+    }
+  }
+
+  React.useEffect(() => {
+    handleGithubResponse();
+  }, [githubResponse]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        disabled={!githubRequest}
+        title="Login"
+        onPress={() => {
+          promptGithubAsync();
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +71,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
