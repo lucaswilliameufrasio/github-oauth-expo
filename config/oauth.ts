3eacae60379960949b6a48eb669e26538d33c189
Lucas William <lucaswilliameufrasio@gmail.com>
2020-08-02 22:58:29 -0300

    chore: add oauth config file to centralize all providers

---
+++ config/oauth.ts
@@ -0,0 +1,16 @@
+export default {
+  github: {
+    config: {
+      clientId: "356660bcfa4691481031",
+      scopes: ["identity"],
+      // For usage in managed apps using the proxy
+      redirectUri: "",
+    },
+    discovery: {
+      authorizationEndpoint: "https://github.com/login/oauth/authorize",
+      tokenEndpoint: "https://github.com/login/oauth/access_token",
+      revocationEndpoint:
+        "https://github.com/settings/connections/applications/356660bcfa4691481031",
+    },
+  },
+};
